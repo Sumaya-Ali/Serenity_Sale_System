1157de5e0349d8756d43ba10d1bd121a238e936f
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2022-04-22 17:40:20 +0300

    PersistingSettingsToDatabase- UserPreferencesTable

--- Serenity_Sale_System.Web/Modules/Common/ScriptInitialization.ts
+++ Serenity_Sale_System.Web/Modules/Common/ScriptInitialization.ts
@@ -1,8 +1,25 @@
 ﻿/// <reference path="../Common/Helpers/LanguageList.ts" />
 
 namespace Serenity_Sale_System.ScriptInitialization {
+
+    /*
+    export class UserLocalStorage implements Serenity.SettingStorage {
+        getItem(key: string): string {
+            return window.localStorage.getItem(
+                Authorization.userDefinition.Username + ":" + key);
+        }
+
+        setItem(key: string, value: string): void {
+            window.localStorage.setItem(
+                Authorization.userDefinition.Username + ":" + key, value);
+        }
+    }
+    */
     Q.Config.responsiveDialogs = true;
     Q.Config.rootNamespaces.push('Serenity_Sale_System');
+  //  Serenity.DataGrid.defaultPersistanceStorage = window.sessionStorage; //window.localStorage
+ //   Serenity.DataGrid.defaultPersistanceStorage = new UserLocalStorage();
+    Serenity.DataGrid.defaultPersistanceStorage = new Serenity.Extensions.UserPreferenceStorage();
     Serenity.EntityDialog.defaultLanguageList = LanguageList.getValue;
     Serenity.HtmlContentEditor.CKEditorBasePath = "~/Serenity.Assets/Scripts/ckeditor/";
 
